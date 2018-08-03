3f06348eabd406541a787a8d3edc0a73a80f6307
Matthew Yuen <un.matthew@gmail.com>
2018-08-03 00:12:48 -0400

    Completed Chapter 3 exercise

--- Chapter_3/load_display_save.py
+++ Chapter_3/load_display_save.py
@@ -5,3 +5,16 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "Path to the image")
 args = vars(ap.parse_args())
+
+# Loading image
+image = cv2.imread(args["image"])
+print("width: {} pixels".format(image.shape[1]))
+print("height: {} pixels".format(image.shape[0]))
+print("channels: {}".format(image.shape[2]))
+
+# Displaying image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+# Saving image
+cv2.imwrite("newimage.jpg", image)
