6f67ea99b3927c387e4ee3b1bd678395b897f9c2
Matthew Yuen <un.matthew@gmail.com>
2018-08-23 00:37:35 -0400

    Added adaptive thresholding

--- Chapter_9/adaptive_thresholding.py
+++ Chapter_9/adaptive_thresholding.py
@@ -3,3 +3,17 @@ import cv2
 
 # Loading image
 image = cv2.imread("coins.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imshow("Blurred Image", blurred)
+
+# Applying adaptive thresholding
+meanThresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+cv2.imshow("Mean Thresh", meanThresh)
+
+gaussianThresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 3)
+cv2.imshow("Gaussian Thresh", gaussianThresh)
+
+cv2.imshow("Comparison", np.hstack([blurred, meanThresh, gaussianThresh]))
+
+cv2.waitKey(0)
