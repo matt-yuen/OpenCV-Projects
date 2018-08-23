0e0f95d700e85d6701a36476f86b95535fe84931
Matthew Yuen <un.matthew@gmail.com>
2018-08-23 00:20:59 -0400

    Added comments

---
+++ Chapter_9/adaptive_thresholding.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2
+
+# Loading image
+image = cv2.imread("coins.jpg")
--- Chapter_9/simple_thresholding.py
+++ Chapter_9/simple_thresholding.py
@@ -7,12 +7,15 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 cv2.imshow("Blurred Image", blurred)
 
+# Performing binary threshold
 (T, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
 cv2.imshow("Threshold Binary", thresh)
 
+# Performing inverse binary threshold
 (T, threshInv) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY_INV)
 cv2.imshow("Threshold Binary Inverse", threshInv)
 
+# Finding coins in the image
 coins = cv2.bitwise_and(image, image, mask = threshInv)
 cv2.imshow("Coins", coins)
 
